6d97404d2f81cd3c2e833120bec1b91eca6001af
Liz <liz@Liz-s-Mac.local>
2020-10-21 12:00:18 -0500

    print 'add pin to board form' on collapse button click

--- src/javascripts/components/forms/addPinToBoardForm.js
+++ src/javascripts/components/forms/addPinToBoardForm.js
@@ -6,8 +6,15 @@ const addPinToBoardForm = () => {
           </button>
         </div>
         <div class="collapse" id="collapseForm">
-          <div class="card card-body">
-            Form here pls
+          <div class="mt-3" id="collapse-pin-form-container">
+            <h2>Add a Pin</h2>
+            <div id="success-message"></div>
+            <div id="error-message"></div>
+            <div class="form-group row">
+              <label for="pinImageUrl">Image</label>
+              <input type="text" class="form-control" id="pin-image-url" placeholder="Place Image URL">
+            </div>
+            <button id="add-pin-btn" type="submit" class="btn form-btn"><i class="fas fa-plus-circle"></i> Add Pin to Board</button>
           </div>
         </div>`;
   return domString;
